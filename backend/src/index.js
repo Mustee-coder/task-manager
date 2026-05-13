7a169d029a830bd200a7a2c7f91b3f71846c9650
Mujittapha Magaji <magajimujittpha5@gmail.com>
2026-05-13 14:52:55 +0100

    ready for railway deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 // Enable CORS
 app.use(
   cors({
-    origin: "http://localhost:5174", 
+    origin: "http://localhost:5173", 
     methods: ["GET", "POST", "PUT", "DELETE","PATCH"],
     credentials: true,
   })
